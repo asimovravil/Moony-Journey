45f673e79984e5b1aa746495171c8932003d7a18
Ravil <star.toni@mail.ru>
2023-09-20 17:17:46 +0600

    [TASK] Edits SettingsController

--- Moony Journey/Controllers/SettingsModule/SettingsViewController.swift
+++ Moony Journey/Controllers/SettingsModule/SettingsViewController.swift
@@ -32,12 +32,14 @@ final class SettingsViewController: UIViewController {
     private lazy var shareAppButton: UIButton = {
         let button = UIButton()
         button.setImage(AppImage.shareApp.uiImage, for: .normal)
+        button.addTarget(self, action: #selector(shareAppButtonTapped), for: .touchUpInside)
         return button
     }()
     
     private lazy var rateUsButton: UIButton = {
         let button = UIButton()
         button.setImage(AppImage.rateUs.uiImage, for: .normal)
+        button.addTarget(self, action: #selector(rateUsButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -91,4 +93,75 @@ final class SettingsViewController: UIViewController {
             make.height.equalTo(74)
         }
     }
+    
+    // MARK: - shareAppButtonTapped
+    
+    @objc private func shareAppButtonTapped() {
+        let actionSheet = UIAlertController(title: nil, message: "Share App", preferredStyle: .actionSheet)
+        let messageAction = UIAlertAction(title: "Share via Message", style: .default) { [weak self] _ in
+            self?.shareAppViaMessage()
+        }
+
+        let otherAppsAction = UIAlertAction(title: "Share via Other Apps", style: .default) { [weak self] _ in
+            self?.shareAppViaOtherApps()
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        actionSheet.addAction(messageAction)
+        actionSheet.addAction(otherAppsAction)
+        actionSheet.addAction(cancelAction)
+
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+    private func shareAppViaMessage() {
+    }
+
+    private func shareAppViaOtherApps() {
+        let textToShare = "Check out this awesome app!"
+        let appURL = URL(string: "https://www.yourappstorelink.com")!
+
+        let activityViewController = UIActivityViewController(activityItems: [textToShare, appURL], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: - rateUsButtonTapped
+    
+    @objc private func rateUsButtonTapped() {
+        let appName = "Moony Journey"
+
+        if let url = URL(string: "https://itunes.apple.com/search?term=\(appName)&entity=software") {
+            let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    return
+                }
+                if let data = data {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                           let results = json["results"] as? [[String: Any]],
+                           let firstResult = results.first,
+                           let appID = firstResult["trackId"] as? Int {
+                            self.openAppStorePage(for: appID)
+                        }
+                    } catch {
+                        print("JSON parsing error: \(error.localizedDescription)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
+    private func openAppStorePage(for appID: Int) {
+        let appStoreURL = URL(string: "itms-apps://itunes.apple.com/app/id\(appID)?action=write-review")!
+
+        if UIApplication.shared.canOpenURL(appStoreURL) {
+            UIApplication.shared.open(appStoreURL, options: [:], completionHandler: nil)
+        } else {
+            print("Unable to open App Store URL")
+        }
+    }
+    
 }
