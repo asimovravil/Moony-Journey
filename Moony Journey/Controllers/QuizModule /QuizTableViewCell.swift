b8c6430e8bf141359d1431c9763e591a539a94a7
Ravil <star.toni@mail.ru>
2023-10-02 18:09:04 +0600

    [TASK] QuizTableViewCell

---
+++ Moony Journey/Controllers/QuizModule /QuizTableViewCell.swift
@@ -0,0 +1,248 @@
+//
+//  QuizTableViewCell.swift
+//  Moony Journey
+//
+//  Created by Ravil on 02.10.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class QuizTableViewCell: UITableViewCell {
+
+    static let reuseID = String(describing: QuizTableViewCell.self)
+    var quizBrain = QuizBrain()
+    private var answerSelected = false
+    var userCorrectAnswers = 0
+    weak var navigationController: UINavigationController?
+
+    // MARK: - UI
+    
+    private lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "01/10"
+        label.textColor = AppColor.green.uiColor
+        label.font = UIFont(name: "Heebo-Medium", size: 14)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var questionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "What is the 6th planet in the Solar system?"
+        label.textColor = AppColor.white.uiColor
+        label.font = UIFont(name: "Heebo-Regular", size: 28)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var questionImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.question.uiImage
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var firstAnswerButton: UIButton = {
+        let button = UIButton()
+        button.setImage(AppImage.firstAnswer.uiImage, for: .normal)
+        button.setTitle("Jupiter", for: .normal)
+        button.setTitleColor(AppColor.white.uiColor, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
+        button.contentHorizontalAlignment = .left
+        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        return button
+    }()
+    
+    private lazy var secondAnswerButton: UIButton = {
+        let button = UIButton()
+        button.setImage(AppImage.secondAnswer.uiImage, for: .normal)
+        button.setTitle("Saturn", for: .normal)
+        button.setTitleColor(AppColor.white.uiColor, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
+        button.contentHorizontalAlignment = .left
+        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        return button
+    }()
+    
+    private lazy var thirdAnswerButton: UIButton = {
+        let button = UIButton()
+        button.setImage(AppImage.thirdAnswer.uiImage, for: .normal)
+        button.setTitle("Earth", for: .normal)
+        button.setTitleColor(AppColor.white.uiColor, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
+        button.contentHorizontalAlignment = .left
+        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        return button
+    }()
+    
+    private lazy var fourthAnswerButton: UIButton = {
+        let button = UIButton()
+        button.setImage(AppImage.fourthAnswer.uiImage, for: .normal)
+        button.setTitle("Neptune", for: .normal)
+        button.setTitleColor(AppColor.white.uiColor, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
+        button.contentHorizontalAlignment = .left
+        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        return button
+    }()
+    
+    private lazy var quitQuizButton: UIButton = {
+        let button = UIButton()
+        button.setImage(AppImage.quitQuiz.uiImage, for: .normal)
+        button.addTarget(self, action: #selector(quitQuizButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var nextQuizButton: UIButton = {
+        let button = UIButton()
+        button.setImage(AppImage.nextQuiz.uiImage, for: .normal)
+        button.addTarget(self, action: #selector(nextQuizButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupConstraints()
+        updateUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - setupViews
+    
+    private func setupViews() {
+        [countLabel, questionLabel, questionImage, firstAnswerButton, secondAnswerButton, thirdAnswerButton, fourthAnswerButton, quitQuizButton, nextQuizButton].forEach() {
+            contentView.addSubview($0)
+        }
+    }
+    
+    // MARK: - setupConstraints
+    
+    private func setupConstraints() {
+        countLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(70)
+            make.leading.equalToSuperview().offset(24)
+        }
+        questionLabel.snp.makeConstraints { make in
+            make.top.equalTo(countLabel.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.bottom.equalTo(questionImage.snp.top).offset(-16)
+        }
+        questionImage.snp.makeConstraints { make in
+            make.top.equalTo(questionLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+        }
+        firstAnswerButton.snp.makeConstraints { make in
+            make.top.equalTo(questionImage.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(36)
+            make.height.equalTo(60)
+            make.width.equalTo(342)
+        }
+        secondAnswerButton.snp.makeConstraints { make in
+            make.top.equalTo(firstAnswerButton.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(36)
+            make.height.equalTo(60)
+            make.width.equalTo(342)
+        }
+        thirdAnswerButton.snp.makeConstraints { make in
+            make.top.equalTo(secondAnswerButton.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(36)
+            make.height.equalTo(60)
+            make.width.equalTo(342)
+        }
+        fourthAnswerButton.snp.makeConstraints { make in
+            make.top.equalTo(thirdAnswerButton.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(36)
+            make.height.equalTo(60)
+            make.width.equalTo(342)
+        }
+        quitQuizButton.snp.makeConstraints { make in
+            make.top.equalTo(fourthAnswerButton.snp.bottom).offset(28)
+            make.leading.equalToSuperview().offset(24)
+            make.height.equalTo(49)
+        }
+        nextQuizButton.snp.makeConstraints { make in
+            make.top.equalTo(fourthAnswerButton.snp.bottom).offset(28)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(49)
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func quitQuizButtonTapped() {
+        let controller = TabBarController()
+        controller.navigationItem.hidesBackButton = true
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @objc private func nextQuizButtonTapped() {
+        quizBrain.nextQuestion()
+        
+        if quizBrain.questionNumber == 0 {
+            showResultViewController()
+        } else {
+            updateUI()
+        }
+    }
+    
+    @objc public func updateUI() {
+        let questionText = quizBrain.getQuestionText()
+        let answers = quizBrain.getAnswers()
+        
+        questionLabel.text = questionText
+        firstAnswerButton.setTitle(answers[0], for: .normal)
+        secondAnswerButton.setTitle(answers[1], for: .normal)
+        thirdAnswerButton.setTitle(answers[2], for: .normal)
+        fourthAnswerButton.setTitle(answers[3], for: .normal)
+        
+        countLabel.text = "\(quizBrain.questionNumber + 1)/\(quizBrain.quiz.count)"
+        
+        firstAnswerButton.backgroundColor = UIColor.clear
+        secondAnswerButton.backgroundColor = UIColor.clear
+        thirdAnswerButton.backgroundColor = UIColor.clear
+        fourthAnswerButton.backgroundColor = UIColor.clear
+        
+        answerSelected = false
+    }
+    
+    @objc private func answerButtonTapped(_ sender: UIButton) {
+        if !answerSelected {
+            let userAnswer = sender.currentTitle!
+
+            let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
+
+            if userGotItRight {
+                sender.backgroundColor = AppColor.green.uiColor
+                userCorrectAnswers += 10
+            } else {
+                sender.backgroundColor = AppColor.red.uiColor
+            }
+
+            sender.layer.cornerRadius = 10
+
+            answerSelected = true
+        }
+    }
+    
+    private func showResultViewController() {
+        let resultViewController = ResultViewController()
+        resultViewController.userCorrectAnswers = userCorrectAnswers
+        self.navigationController?.pushViewController(resultViewController, animated: true)
+    }
+}
+
--- Moony Journey/Controllers/QuizModule /QuizViewController.swift
+++ Moony Journey/Controllers/QuizModule /QuizViewController.swift
@@ -9,99 +9,20 @@ import UIKit
 import SnapKit
 
 final class QuizViewController: UIViewController {
-
-    var quizBrain = QuizBrain()
-    private var answerSelected = false
-    var userCorrectAnswers = 0
     
     // MARK: - UI
     
-    private lazy var countLabel: UILabel = {
-        let label = UILabel()
-        label.text = "01/10"
-        label.textColor = AppColor.green.uiColor
-        label.font = UIFont(name: "Heebo-Medium", size: 14)
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private lazy var questionLabel: UILabel = {
-        let label = UILabel()
-        label.text = "What is the 6th planet in the Solar system?"
-        label.textColor = AppColor.white.uiColor
-        label.font = UIFont(name: "Heebo-Regular", size: 28)
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private lazy var questionImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = AppImage.question.uiImage
-        imageView.layer.masksToBounds = true
-        imageView.contentMode = .scaleAspectFill
-        return imageView
-    }()
-    
-    private lazy var quitQuizButton: UIButton = {
-        let button = UIButton()
-        button.setImage(AppImage.quitQuiz.uiImage, for: .normal)
-        button.addTarget(self, action: #selector(quitQuizButtonTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    private lazy var nextQuizButton: UIButton = {
-        let button = UIButton()
-        button.setImage(AppImage.nextQuiz.uiImage, for: .normal)
-        button.addTarget(self, action: #selector(nextQuizButtonTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    private lazy var firstAnswerButton: UIButton = {
-        let button = UIButton()
-        button.setImage(AppImage.firstAnswer.uiImage, for: .normal)
-        button.setTitle("Jupiter", for: .normal)
-        button.setTitleColor(AppColor.white.uiColor, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
-        button.contentHorizontalAlignment = .left
-        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-        return button
-    }()
-    
-    private lazy var secondAnswerButton: UIButton = {
-        let button = UIButton()
-        button.setImage(AppImage.secondAnswer.uiImage, for: .normal)
-        button.setTitle("Saturn", for: .normal)
-        button.setTitleColor(AppColor.white.uiColor, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
-        button.contentHorizontalAlignment = .left
-        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-        return button
-    }()
-    
-    private lazy var thirdAnswerButton: UIButton = {
-        let button = UIButton()
-        button.setImage(AppImage.thirdAnswer.uiImage, for: .normal)
-        button.setTitle("Earth", for: .normal)
-        button.setTitleColor(AppColor.white.uiColor, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
-        button.contentHorizontalAlignment = .left
-        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-        return button
-    }()
-    
-    private lazy var fourthAnswerButton: UIButton = {
-        let button = UIButton()
-        button.setImage(AppImage.fourthAnswer.uiImage, for: .normal)
-        button.setTitle("Neptune", for: .normal)
-        button.setTitleColor(AppColor.white.uiColor, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Heebo-Bold", size: 24)
-        button.contentHorizontalAlignment = .left
-        button.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-        return button
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(QuizTableViewCell.self, forCellReuseIdentifier: QuizTableViewCell.reuseID)
+        tableView.layer.cornerRadius = 26
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .clear
+        tableView.rowHeight = 850
+        tableView.showsVerticalScrollIndicator = false
+        tableView.separatorStyle = .none
+        return tableView
     }()
     
     // MARK: - Lifecycle
@@ -112,13 +33,12 @@ final class QuizViewController: UIViewController {
         setupViews()
         setupConstraints()
         setupNavigationBar()
-        updateUI()
     }
     
     // MARK: - setupViews
     
     private func setupViews() {
-        [countLabel, questionLabel, questionImage, quitQuizButton, nextQuizButton, firstAnswerButton, secondAnswerButton, thirdAnswerButton, fourthAnswerButton].forEach() {
+        [tableView].forEach() {
             view.addSubview($0)
         }
         view.backgroundColor = AppColor.backgroundColor.uiColor
@@ -127,54 +47,8 @@ final class QuizViewController: UIViewController {
     // MARK: - setupConstraints
     
     private func setupConstraints() {
-        countLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(132)
-            make.leading.equalToSuperview().offset(24)
-        }
-        questionLabel.snp.makeConstraints { make in
-            make.top.equalTo(countLabel.snp.bottom).offset(8)
-            make.leading.equalToSuperview().offset(24)
-            make.trailing.equalToSuperview().offset(-24)
-            make.bottom.equalTo(questionImage.snp.top).offset(-16)
-        }
-        questionImage.snp.makeConstraints { make in
-            make.top.equalTo(questionLabel.snp.bottom).offset(16)
-            make.leading.equalToSuperview().offset(24)
-            make.trailing.equalToSuperview().offset(-24)
-        }
-        firstAnswerButton.snp.makeConstraints { make in
-            make.top.equalTo(questionImage.snp.bottom).offset(16)
-            make.leading.equalToSuperview().offset(36)
-            make.height.equalTo(60)
-            make.width.equalTo(342)
-        }
-        secondAnswerButton.snp.makeConstraints { make in
-            make.top.equalTo(firstAnswerButton.snp.bottom).offset(8)
-            make.leading.equalToSuperview().offset(36)
-            make.height.equalTo(60)
-            make.width.equalTo(342)
-        }
-        thirdAnswerButton.snp.makeConstraints { make in
-            make.top.equalTo(secondAnswerButton.snp.bottom).offset(8)
-            make.leading.equalToSuperview().offset(36)
-            make.height.equalTo(60)
-            make.width.equalTo(342)
-        }
-        fourthAnswerButton.snp.makeConstraints { make in
-            make.top.equalTo(thirdAnswerButton.snp.bottom).offset(8)
-            make.leading.equalToSuperview().offset(36)
-            make.height.equalTo(60)
-            make.width.equalTo(342)
-        }
-        quitQuizButton.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(24)
-            make.bottom.equalToSuperview().offset(-43)
-            make.height.equalTo(49)
-        }
-        nextQuizButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().offset(-24)
-            make.bottom.equalToSuperview().offset(-43)
-            make.height.equalTo(49)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
     }
     
@@ -207,67 +81,24 @@ final class QuizViewController: UIViewController {
         let controller = HintViewController()
         self.navigationController?.pushViewController(controller, animated: true)
     }
-    
-    // MARK: - Actions
-    
-    @objc private func quitQuizButtonTapped() {
-        let controller = TabBarController()
-        controller.navigationItem.hidesBackButton = true
-        self.navigationController?.pushViewController(controller, animated: true)
+}
+
+extension QuizViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
-    @objc private func nextQuizButtonTapped() {
-        quizBrain.nextQuestion()
-        
-        if quizBrain.questionNumber == 0 {
-            showResultViewController()
-        } else {
-            updateUI()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: QuizTableViewCell.reuseID, for: indexPath) as? QuizTableViewCell else {
+            fatalError("Could not cast to QuizTableViewCell")
         }
+        cell.navigationController = self.navigationController
+        cell.selectionStyle = .none
+        cell.backgroundColor = .clear
+        return cell
     }
     
-    @objc private func answerButtonTapped(_ sender: UIButton) {
-        if !answerSelected {
-            let userAnswer = sender.currentTitle!
-
-            let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
-
-            if userGotItRight {
-                sender.backgroundColor = AppColor.green.uiColor
-                userCorrectAnswers += 10
-            } else {
-                sender.backgroundColor = AppColor.red.uiColor
-            }
-
-            sender.layer.cornerRadius = 10
-
-            answerSelected = true
-        }
-    }
-    
-    private func showResultViewController() {
-        let resultViewController = ResultViewController()
-        resultViewController.userCorrectAnswers = userCorrectAnswers
-        self.navigationController?.pushViewController(resultViewController, animated: true)
-    }
-    
-    @objc private func updateUI() {
-        let questionText = quizBrain.getQuestionText()
-        let answers = quizBrain.getAnswers()
-        
-        questionLabel.text = questionText
-        firstAnswerButton.setTitle(answers[0], for: .normal)
-        secondAnswerButton.setTitle(answers[1], for: .normal)
-        thirdAnswerButton.setTitle(answers[2], for: .normal)
-        fourthAnswerButton.setTitle(answers[3], for: .normal)
-        
-        countLabel.text = "\(quizBrain.questionNumber + 1)/\(quizBrain.quiz.count)"
-        
-        firstAnswerButton.backgroundColor = UIColor.clear
-        secondAnswerButton.backgroundColor = UIColor.clear
-        thirdAnswerButton.backgroundColor = UIColor.clear
-        fourthAnswerButton.backgroundColor = UIColor.clear
-        
-        answerSelected = false
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
